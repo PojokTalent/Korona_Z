690ea8dc440829a2ee3962aa880bc6d35c343278
lasiest <valenstiowiyogo@gmail.com>
2020-11-23 15:07:58 +0700

    Menu
    Play berfungsi
    Quit berfungsi

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Playgame ()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+    public void Quitgame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
