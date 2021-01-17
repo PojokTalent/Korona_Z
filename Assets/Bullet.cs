9b432d394be5f080d1a085897b7f9694487a254c
lasiest <valenstiowiyogo@gmail.com>
2021-01-17 20:30:48 +0700

    Update GunFunction
    Bullet
    -Bullet Autodestroyed after 5s
    -Bullet Autodestroyed after collide with something (Enemy include)
    
    Enemy cannot die for now (FOR NOW)

---
+++ Assets/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    void Update()
+    {
+        Destroy(gameObject, 5);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Destroy(collision.collider.gameObject);
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Player/Rotate360.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate360 : MonoBehaviour
+{
+    public Camera cam;
+    //    public Rigidbody2D rb;
+
+    Vector2 mousePos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 lookDir = mousePos;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        //       rb.rotation = angle;
+    }
+}
