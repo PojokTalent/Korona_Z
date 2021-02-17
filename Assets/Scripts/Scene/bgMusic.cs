1203e32eea7b82d5f6338f3416ad4689570f19af
Mr.Miss <keselekpermen@mrmiss.wtf>
2021-02-17 13:56:12 +0700

    Game background music
    Signed-off-by: Mr.Miss <keselekpermen@mrmiss.wtf>

---
+++ Assets/Scripts/Scene/bgMusic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bgMusic : MonoBehaviour
+{
+    public static bgMusic BgInstance;
+
+    private void Awake()
+    {
+        if(BgInstance != null && BgInstance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        BgInstance = this;
+        DontDestroyOnLoad(this);
+    }
+}
