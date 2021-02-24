a6471e647bcafe690f0a8e8d036b55174de98df1
lasiest <valenstiowiyogo@gmail.com>
2021-02-24 16:37:35 +0700

    Deploy Enemy, HP, Ammo, End Scene

---
+++ Assets/Main_menu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_menu : MonoBehaviour
+{
+    public void Playgame()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void Quitgame()
+    {
+        Application.Quit();
+    }
+}
